0d04246a69559c66a337768c3653c87129f18768
thomas <thomas@dalek>
2013-05-13 10:54:18 -0700

    Allow for both input to be streamed in via stdin or read from files given by command line

--- src/Indent.hs
+++ src/Indent.hs
@@ -10,39 +10,67 @@ import Indent.Class
 import Indent.Type.CodeGen
 import System.Environment (getArgs)
 import System.FilePath (takeExtension)
+import System.IO
 
 
 main :: IO ()
 main = do
   args <- getArgs
   let (opts, files) = partition ((== "-") . take 1) args
-  if any (`elem` ["-h", "--help"]) opts || null files
+  if any (`elem` ["-h", "--help"]) opts
     then help
-    else mapM_ go files
+    else if null files
+        then byStdin runIndent
+        else mapM_ (byFile runIndent) files
 
 
 help :: IO ()
 help = mapM_ putStrLn [
-    "Usage: tindent [options] files"
+    "Usage: myindent [options] files"
   , "Description: Indents files based on filetype."
   ]
+
+
+data IndentType
+    = HcCodeGen
+
+
+byStdin :: (IndentType -> String -> String) -> IO ()
+byStdin f = do
+    contents <- getContents
+    putStrLn $ f HcCodeGen contents
+
+
+byFile :: (IndentType -> String -> String) -> FilePath -> IO ()
+byFile f file = do
+    contents <- readFile file
+    mIndentType <- case map toLower $ drop 1 $ takeExtension file of
+        "" -> return Nothing
+        "c" -> return $ Just HcCodeGen
+        "cpp" -> return $ Just HcCodeGen
+        _ -> do
+            hPutStrLn stderr $ "Uknown filetype: Skipping " ++ file
+            return Nothing
+    case mIndentType of
+        Nothing -> return ()
+        Just indentType -> do
+            let newContents = f indentType contents
+            length contents `seq` writeFile file newContents
 
 
 type Indent a = String -> Tagged a String
 
 
-go ::FilePath -> IO ()
-go file = do
-  str <- readFile file
-  str' <- indentIO str
-  length str `seq` writeFile file str'
-  where
-    indentIO = case map toLower $ drop 1 $ takeExtension file of
-      "" -> return
-      "c" -> return . untag . (indent :: Indent CodeGen)
-      "cpp" -> return . untag . (indent :: Indent CodeGen)
-      _ -> \str -> do
-        putStrLn $ "Unknown filetype: Skipping " ++ file
-        return str
+runIndent :: IndentType -> String -> String
+runIndent indentType = case indentType of
+    HcCodeGen -> untag . (indent :: Indent CodeGen)
+
+
+
+
+
+
+
+
 
 
