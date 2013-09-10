10b818ecbc6ba67bc279da614d4705aae0d4e7ac
Thomas Eding <thomas@techsoft3d.com>
2013-09-09 17:27:56 -0700

    Reimplemented code

--- src/Text/Indent/Type/CodeGen.hs
+++ src/Text/Indent/Type/CodeGen.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Text.Indent.Type.CodeGen (
-      CodeGen
-    ) where
+    CodeGen
+) where
 
 
 import Control.Arrow
@@ -10,19 +11,38 @@ import Control.Monad.ListM
 import Control.Monad.State.Strict
 import Data.Char
 import Data.List
+import Data.Maybe
 import Data.Tagged
 import Text.Indent.Class
 
 
-data CodeGen = St {
-      indentAmount :: Int
-    }
+data CodeGen
 
 
-initState :: CodeGen
-initState = St {
-      indentAmount = 0
-    }
+type IndentAmount = Int
+type Line = String
+
+
+data IndentItem
+    = Open
+    | Begin
+    | Brace
+    deriving (Show, Eq)
+
+
+data MalformedStack = MalformedStack
+
+
+data IndentState = IndentState {
+    indentAmount :: IndentAmount,
+    itemStack :: Either MalformedStack [IndentItem]
+}
+
+
+initState :: IndentState
+initState = IndentState {
+    indentAmount = 0,
+    itemStack = Right [] }
 
 
 instance Indenter CodeGen where
@@ -31,29 +51,87 @@ instance Indenter CodeGen where
             wsOp = case mode of
                 DropOldTabs -> dropWs
                 KeepOldTabs -> id
-            tabify str = do
-                n <- tabAmount $ dropWs str
-                return $ replicate n '\t' ++ str
+            tabify line = do
+                newTabAmount <- calculateTabs $ dropWs line
+                return $ replicate newTabAmount '\t' ++ line
 
 
 dropWs :: String -> String
 dropWs = dropWhile isSpace
 
 
-tabAmount :: String -> State CodeGen Int
-tabAmount (stripPrefix "DEFINE" -> Just (stripPrefix "(" . dropWs -> Just rest)) = tabAmount rest
-tabAmount (stripPrefix "HC_Open_" -> Just _) = fmap (subtract 1) $ modifyAmount (+ 1)
-tabAmount (stripPrefix "HC_KOpen_" -> Just _) = fmap (subtract 1) $ modifyAmount (+ 1)
-tabAmount (stripPrefix "HC_Close_" -> Just _) = modifyAmount (subtract 1)
-tabAmount (stripPrefix "HC_Begin_" -> Just _) = fmap (subtract 1) $ modifyAmount (+ 1)
-tabAmount (stripPrefix "HC_End_" -> Just _) = modifyAmount (subtract 1)
-tabAmount _ = gets indentAmount
+lastNonWs :: String -> Maybe Char
+lastNonWs s = case dropWs $ reverse s of
+    [] -> Nothing
+    c : _ -> Just c
 
 
-modifyAmount :: (Int -> Int) -> State CodeGen Int
-modifyAmount f = do
-    modify $ \st -> st { indentAmount = f $ indentAmount st }
-    gets indentAmount
+calculateTabs :: Line -> State IndentState IndentAmount
+calculateTabs line = case line of
+    (stripPrefix "DEFINE" -> Just (stripPrefix "(" . dropWs -> Just rest)) -> calculateTabs rest
+    (isPrefixOf "HC_Open_" -> True) -> do
+        n <- gets indentAmount
+        push Open
+        return n
+    (isPrefixOf "HC_KOpen_" -> True) -> do
+        n <- gets indentAmount
+        push Open
+        return n
+    (isPrefixOf "HC_Close_" -> True) -> do
+        pop Open
+        gets indentAmount
+    (isPrefixOf "HC_Begin_" -> True) -> do
+        n <- gets indentAmount
+        push Begin
+        return n
+    (isPrefixOf "HC_End_" -> True) -> do
+        pop Begin
+        gets indentAmount
+    (lastNonWs -> Just '{') -> do
+        n <- gets indentAmount
+        push Brace
+        return n
+    (isPrefixOf "}" -> True) -> do
+        popTill Brace
+        pop Brace
+        gets indentAmount
+    _ -> gets indentAmount
+
+
+topItem :: State IndentState (Maybe IndentItem)
+topItem = gets $ either (const Nothing) listToMaybe . itemStack
+
+
+push :: IndentItem -> State IndentState ()
+push item = do
+    modify $ \st -> st { itemStack = fmap (item :) $ itemStack st }
+    modify $ \st -> st { indentAmount = indentAmount st + 1 }
+
+
+pop :: IndentItem -> State IndentState ()
+pop item = do
+    mItem <- topItem
+    if Just item == mItem
+        then do
+            modify $ \st -> st { itemStack = fmap tail $ itemStack st }
+            modify $ \st -> st { indentAmount = indentAmount st - 1 }
+        else modify $ \st -> st { itemStack = Left MalformedStack }
+
+
+popTill :: IndentItem -> State IndentState ()
+popTill destItem = do
+    mItem <- topItem
+    case mItem of
+        Nothing -> return ()
+        Just item -> if item == destItem
+            then return ()
+            else do
+                pop item
+                popTill destItem
+
+
+
+
 
 
 
